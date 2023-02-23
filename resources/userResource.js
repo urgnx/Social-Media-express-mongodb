4ed91f369bf99d03915b6dce5ebbc7a44b1b7431
urgnx <redbull25200@gmail.com>
2023-02-23 03:16:09 +0300

    user show and update endpoints + user resource

---
+++ resources/userResource.js
@@ -0,0 +1,8 @@
+class userResource {
+  constructor(user) {
+    this.email = user.email;
+    this.username = user.username;
+  }
+}
+
+module.exports = userResource;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,36 @@
 const router = require("express").Router();
+const User = require("../models/user");
 
-router.get("/", (req, res) => {
-  res.send("userRoute");
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  !user && res.status(404).json({ message: "User not found" });
+
+  var userResource = new (require("../resources/userResource"))(user);
+
+  res.status(200).json({ user: userResource });
+});
+
+router.put("/:id", async (req, res) => {
+  var user = await User.findById(req.params.id);
+  !user && res.status(404).json({ message: "User not found" });
+
+  try {
+    await user.update({
+      $set: {
+        username: req.body.username,
+        email: req.body.email,
+        profilePicture: req.body.profilePicture,
+        coverPicture: req.body.coverPicture,
+        followers: req.body.followers,
+        followings: req.body.followings,
+      },
+    });
+
+    var user = await User.findById(req.params.id);
+    res.status(200).json({ user: user });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 });
 
 module.exports = router;
